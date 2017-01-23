72cb33fce81a5155d8659d904606845fc910295d
Patrick William Haugen <pathaugen@gmail.com>
2017-01-23 02:21:51 -0800

    Adding Travis CI Support
    Adding Travis CI Support

--- main.go
+++ main.go
@@ -3,14 +3,40 @@ package main
 
 import (
 	"fmt"
+	
+	"strconv" // https://golang.org/pkg/strconv/
 )
 
 func main() {
 	
 	fmt.Print("Do it all in Golang!")
-	
 	fmt.Print("\n\n")
 	
-	fmt.Print("Example 1:")
+	
+	// ========== ========== ========== ========== ==========
+	// Simple Loop
+	/*
+	fmt.Print("Simple Loop:" + "\n")
+	sum := 0
+	for i := 0; i < 10; i++ {
+		fmt.Print("interval: " + strconv.Itoa(i) + "\n") // Itoa is shorthand for FormatInt(int64(i), 10)
+		sum += i
+		fmt.Print("sum: " + strconv.Itoa(sum) + "\n")
+	}
+	fmt.Print("Final sum: " + strconv.Itoa(sum))
+	fmt.Print("\n\n")
+	*/
+	// ========== ========== ========== ========== ==========
+	
+	
+	// ========== ========== ========== ========== ==========
+	// Load JSON into Array
+	fmt.Print("Load JSON into Array:" + "\n")
+	// ========== ========== ========== ========== ==========
+	
 	
 }
+
+
+
+
