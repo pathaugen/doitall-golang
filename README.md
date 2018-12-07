7cc2f9403c59350800dfd930b08989e654c0978f
Patrick William Haugen <pathaugen@gmail.com>
2018-12-06 19:03:09 -0800

    WIP

--- README.md
+++ README.md
@@ -5,13 +5,19 @@ Do it all! in Golang
 Description
 -----------
 
-Simple project to provide code to do just about anything in Go.
+Simple project framework to provide code and a baseline to do just about anything in Go.
 
 Quick Start
 ------------
 
+(developing)
+
 Features
 --------
 
+(developing)
+
 External Libraries
 ------------------
+
+(developing)
--- main.go
+++ main.go
@@ -3,9 +3,67 @@ package main
 
 import (
 	"fmt"
-	
+
 	"strconv" // https://golang.org/pkg/strconv/
 )
+
+
+var appinfo = `
+  =======================================
+       _           _  _             _  _
+      | |         (_)| |           | || |
+    __| |  ___     _ | |_     __ _ | || |
+   / _`+"`"+` | / _ \   | || __|   / _`+"`"+` || || |
+  | (_| || (_) |  | || |_   | (_| || || |
+   \__,_| \___/   |_| \__|   \__,_||_||_|
+                             ..in Golang!
+  =======================================
+`
+
+
+// TODO: Read from CHANGELOG.md and output the text
+var changelog = `
+CHANGELOG:
+
+Version 0.0.1 BETA
+ - Splash ASCII
+ - Basic functions
+ - Reading flags from command line
+`
+
+
+var documentation = `
+$ doitall-golang
+  Runs the application.
+
+$ doitall-golang -changelog
+  View the changelog.
+
+---------- Math Operations ----------
+
+  Addition:
+  $ doitall-golang -add X Y
+    Adds the variables X and Y
+
+  Subtraction:
+  $ doitall-golang -sub X Y
+    Adds the variables X and Y
+
+  Multiplication:
+  $ doitall-golang -mult X Y
+    Adds the variables X and Y
+
+  Division:
+  $ doitall-golang -div X Y
+    Adds the variables X and Y
+
+---------- API Operations ----------
+
+  Define API Endpoint:
+  $ doitall-golang -api X
+    Specifies X as the API endpoint to utilize for API operations.
+
+`
 
 
 func add(x int, y int) int {
@@ -14,27 +72,27 @@ func add(x int, y int) int {
 
 
 func main() {
-	
+
 	breakline := ("\n\n" + "========== ========== ========== ========== ==========" + "\n\n")
 	breakspace := ("\n\n")
-	
+
 	fmt.Print(breakspace)
-	
+
 	fmt.Print("Do it all in Golang!")
 	fmt.Print(breakspace)
 	fmt.Print("https://github.com/pathaugen/doitall-golang")
-	
+
 	fmt.Print(breakline)
-	
+
 	// ========== ========== ========== ========== ==========
 	// Simple Addition
 	fmt.Print("Simple Addition:" + breakspace)
 	//fmt.Print("Calling function add with 42, 13: " + add(42, 13))
 	fmt.Print(add(42, 13))
 	// ========== ========== ========== ========== ==========
-	
+
 	fmt.Print(breakline)
-	
+
 	// ========== ========== ========== ========== ==========
 	// Simple Loop
 	fmt.Print("Simple Loop:" + breakspace)
@@ -46,25 +104,21 @@ func main() {
 	}
 	fmt.Print("Final sum: " + strconv.Itoa(sum))
 	// ========== ========== ========== ========== ==========
-	
+
 	fmt.Print(breakline)
-	
+
 	// ========== ========== ========== ========== ==========
 	// Take Command Line Arguments
 	fmt.Print("Take Command Line Arguments:" + breakspace)
 	// ========== ========== ========== ========== ==========
-	
+
 	fmt.Print(breakline)
-	
+
 	// ========== ========== ========== ========== ==========
 	// Load JSON into Array
 	fmt.Print("Load JSON into Array:" + breakspace)
 	// ========== ========== ========== ========== ==========
-	
+
 	fmt.Print(breakline)
-	
+
 }
-
-
-
-
